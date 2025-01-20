9db5b61f6cb0f27407482420ed58253d1fa84167
daye <myannnng@gmail.com>
2025-01-20 17:56:16 +0900

    programmers 1/20

--- problem8.py
+++ problem8.py
@@ -1,3 +1,19 @@
 #홀짝 구분하기
 # 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을,
 #  홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+
+num= int(input())
+
+if num%2==0:
+    print(f"{num} is even")
+else :
+    print(f"{num} is odd ")
+
+
+
+
+
+
+# N = int(input())
+# print(f"{N} is {'even' if N % 2 == 0 else 'odd'}")
+# 출력 값 같음 
