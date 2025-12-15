81a31808c46d618d260fbcbea2587213e24f7acd
daye <myannnng@gmail.com>
2025-12-15 21:02:52 +0900

    Solving

---
+++ src/baekjoon/p2164/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.p2164;
+
+// 카드2 백준 2164 문제
+// 배열 선언하고 로직 반복
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String problem = br.readLine();
+        int n = Integer.parseInt(problem);
+
+        int[] numberCard = new int[n];
+
+    }
+}
