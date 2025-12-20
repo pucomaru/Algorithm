c263fd8b79f39ec63b8ebb4ca3ffa190a205e6a6
daye <myannnng@gmail.com>
2025-12-20 20:19:31 +0900

    Solved

--- src/baekjoon/p2164/시간초과.java
+++ src/baekjoon/p2164/시간초과.java
@@ -2,11 +2,15 @@ package baekjoon.p2164;
 
 // 카드2 백준 2164 문제
 // 배열 선언하고 로직 반복
-// 시간 초과..
+
+//배열 방식은 맨 앞 카드를 버리거나 뒤로 보내기 위해 매번 남은 카드 전체를 한 칸씩 이
+//동시키는 작업이 필요하고, 이 과정이 카드 개수만큼 반복되면서 연산량이 크게 증가해
+//시간 복잡도가 N²이 되어 시간 초과가 발생한다.
+
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-public class Main {
+public class 시간초과 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -40,3 +44,4 @@ public class Main {
 
     }
 }
+
---
+++ src/baekjoon/p2164/카드2.java
@@ -0,0 +1,38 @@
+package baekjoon.p2164;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+// 큐 방식은 맨 앞 원소를 제거하거나 뒤에 추가할 떄 실제로 모든 원소를 이동시키지 않고,
+// 맨 앞과 맨 뒤를 가리키는 위치만 변경한다. poll(),add() 는 카드들을 한 칸씩 밀어내는
+// 작업이 아니라 인덱스(포인터)만 이동하는 연선이어서 한 번에 처리되는 비용이 매우 작다.
+
+public class 카드2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 카드들
+        Queue<Integer> cardList = new ArrayDeque<>();
+
+        // 카드 갯수
+        int numberOfCards = Integer.parseInt(br.readLine());
+
+        // 카드에 1 ~ N 넣기
+        for (int i = 0; i < numberOfCards; i++){
+            cardList.add(i+1);
+        }
+
+        // 카드 맨앞 카드 제거후 다음 카드 뒤로 옮기기 반복 작업
+        while (cardList.size() > 1){
+            cardList.poll();
+            cardList.add(cardList.poll());
+        }
+
+        System.out.println(cardList.peek());
+
+
+    }
+}
